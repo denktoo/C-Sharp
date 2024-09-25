6320e51bb82d0e819a9519f01579e7cd2ce78117
denistoo749 <denis.k.too@gmail.com>
2024-09-25 14:53:40 +0300

    LINQ and collections

---
+++ repos/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ShowLINQ();
+    }
+
+    private static void ShowLINQ()
+    {
+        List<Employee> employee = BuildList();
+
+        // LINQ Query.
+        var subset = from emp in employee
+                     where emp.Age > 20
+                     orderby emp.Name
+                     select emp;
+
+        foreach (Employee emp in subset)
+        {
+            Console.WriteLine(emp.Name + " " + emp.Age);
+        }
+    }
+
+    private static List<Employee> BuildList() => new()
+        {
+            { new Employee() { Name = "Sambit", Age = 30, Gender = "Male" } },
+            { new Employee() { Name = "Priyanka", Age = 40, Gender = "Female" }},
+            { new Employee() { Name = "Pranaya", Age = 34, Gender = "Male" }},
+            { new Employee() { Name = "Anurag", Age = 38, Gender = "Female" }}
+        };
+
+}
+public class Employee
+{
+    public required string Name { get; init; }
+    public required int Age { get; init; }
+    public required string Gender { get; init; }
+}
