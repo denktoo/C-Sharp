4236169fcc5a74648ca6f5dc9cdf650e1990fa70
denktoo <146120658+denktoo@users.noreply.github.com>
2025-01-09 13:16:44 +0300

    Refactor registration with Knockoutjs

--- FitTrack/wwwroot/js/Login.js
+++
@@ -1,8 +0,0 @@
-﻿function LoginViewModel() {
-}
-
-// Expose the view model to the global scope
-window.loginViewModel = new LoginViewModel();
-
-// Apply Knockout bindings
-ko.applyBindings(loginViewModel);
